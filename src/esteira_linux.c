13c290a29433835e290b8a2e6584ae39e008f95a
berna <adramalec@gmail.com>
2025-12-04 00:19:19 -0300

    Fix variable initialization for stdin flags restoration in task_input function

--- src/esteira_linux.c
+++ src/esteira_linux.c
@@ -491,7 +491,7 @@ static void *task_input(void *arg) {
     }
     
     // Restaura stdin bloqueante ao sair
-    int flags = fcntl(STDIN_FILENO, F_GETFL, 0);
+    flags = fcntl(STDIN_FILENO, F_GETFL, 0);
     fcntl(STDIN_FILENO, F_SETFL, flags & ~O_NONBLOCK);
     
     return NULL;
